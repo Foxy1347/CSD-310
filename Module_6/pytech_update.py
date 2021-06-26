abd21dd30a2d7aac5a1ebab7e355cc0982d9c07a
Foxy1347 <foxshanem@gmail.com>
2021-06-26 11:32:08 -0500

    Uploading python update

---
+++ Module_6/pytech_update.py
@@ -0,0 +1,18 @@
+"""Title: pytech_update.py
+    Author: Shane Fox
+    Date: 6/26/2021
+    Description: Test program for updating documents to the students collection."""
+
+""" Import modules """
+import pymongo
+from pymongo import MongoClient
+
+# Connection string
+url = "mongodb+srv://admin:admin@cluster0.mdwlq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+# Connect to the cluster
+client = MongoClient(url)
+
+# Connect pytech database
+db = client.pytech
+
