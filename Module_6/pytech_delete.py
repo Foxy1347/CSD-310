0afff8f10dcd0a601c786743a283e45828722814
Foxy1347 <foxshanem@gmail.com>
2021-06-26 12:26:56 -0500

    Added student info

--- Module_6/pytech_delete.py
+++ Module_6/pytech_delete.py
@@ -25,6 +25,34 @@ for doc in docs:
     print("  Student ID: " + doc["student_id"] + "\n  First Name: " + doc["first_name"] + "\n  Last Name: " + doc["last_name"] + "\n")
 
 # Insert new student
+feanor =  {
+    "student_id": "1010",
+    "first_name": "Feanor",
+    "last_name": "Curufinwe",
+    "enrollments": [
+        {
+            "term": "Session 2",
+            "gpa": "4.0",
+            "start_date": "July 10, 2020",
+            "end_date": "September 14, 2020",
+            "courses": [
+                {
+                    "course_id": "CSD310",
+                    "description": "Database Development and Use",
+                    "instructor": "Professor Krasso",
+                    "grade": "A+"
+                },
+                {
+                    "course_id": "CSD320",
+                    "description": "Programming with Java",
+                    "instructor": "Professor Krasso",
+                    "grade": "A+"
+                }
+            ]
+        }
+    ]
+}
+
 
 
 # show an exit message
